7139c3cef379aa93a23aa84a38c2d8226dc05d7f
Nikhil dangi <nikhildangi1205@gmail.com>
2026-07-16 18:27:08 +0530

    Day 2

--- BTaxi.java
+++ BTaxi.java
@@ -5,7 +5,6 @@ public class BTaxi{
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] arr=new int[n];
-        
         int count=0;
         
         for(int i=0;i<n;i++){
