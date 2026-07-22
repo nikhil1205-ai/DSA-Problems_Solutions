b44e5d3e36d9dfe2716ca5446b5871af0dfdb058
Nikhil dangi <nikhildangi1205@gmail.com>
2026-07-22 23:40:15 +0530

    Day 7

--- BKefaAndCompany.java
+++ BKefaAndCompany.java
@@ -1,22 +1,41 @@
 import java.util.*;
 
 public class BKefaAndCompany {
-    
+    static class Friend{
+        long money,factor;
+        Friend(long money,long factor){
+            this.money=money;
+            this.factor=factor;
+        }
+    }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int friend= sc.nextInt();
-        int budget = sc.nextInt();
+        int friends=sc.nextInt();
+        long diff=sc.nextLong();
         
-        int sum=0;
-        for(int i=0;i<friend;i++){
-            int a= sc.nextInt();
-            int b = sc.nextInt();
-            if(a-budget>) sum+= b;
-            
+        Friend[] arr=new Friend[friends];
+        
+        for(int i=0;i<friends;i++){
+            arr[i] = new Friend(sc.nextLong(),sc.nextLong());
         }
-        System.out.println(sum);
+        
+        Arrays.sort(arr,Comparator.comparingLong(a-> a.money));
+        
+        int left=0;
+        Long sum=Long.valueOf(0),ans=Long.valueOf(0);
         
         
+        for(int right=0;right<friends;right++){
+            sum+=arr[right].factor;
+            
+            while(arr[right].money-arr[left].money>=diff){
+                sum-=arr[left].factor;
+                left++;
+            }
+            ans=Math.max(ans,sum);
+        }
+        
+        System.out.println(ans);
         
         
     }
