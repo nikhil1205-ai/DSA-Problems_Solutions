82e34b2f3501815a05032ef54bbfab5f46a5970d
Nikhil dangi <nikhildangi1205@gmail.com>
2026-08-01 19:25:05 +0530

    Day 12

---
+++ LeetCode_50.java
@@ -0,0 +1,22 @@
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+class Solution {
+    public double myPow(double x, int n) {
+        boolean exp=false;
+        long N=n;
+        if (N<0) {
+            exp=true;
+            N=-(N);
+        }
+        double ans=1;
+        while(N>0){
+            if((N&1)!=0) ans=ans*x;
+            x=x*x;
+            N>>=1;
+        }
+
+        if(exp) ans=1/ans;
+        return ans;
+
+    }
+}
